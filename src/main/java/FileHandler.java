82e5303f23f0d733d57ce372d77c1e253c0684be
RawanHakami <rhakami0060@stu.kau.edu.sa>
2025-04-26 01:26:39 +0300

    Add files via upload

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -10,6 +10,10 @@ public class FileHandler {
     private static ArrayList<Form> savedForms = new ArrayList<>();
 
     
+    public ArrayList<Form> getSavedForms() {
+    return savedForms;
+}
+
     public static void save(Form form) {
         savedForms.add(form);
         System.out.println("Form saved successfully.");
